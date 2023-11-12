15e89c23c2373893e732ff72f70b8b36bbb115c2
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-11-12 20:23:47 +0100

    Funcionalidad boton 2048

--- app/src/main/java/com/example/androidgames/MainActivity.java
+++ app/src/main/java/com/example/androidgames/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.androidgames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStartGame2048).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGameActivity();
+            }
+        });
+    }
+
+    private void startGameActivity() {
+        Intent intent = new Intent(this, Game2048.class);
+        startActivity(intent);
     }
 }
