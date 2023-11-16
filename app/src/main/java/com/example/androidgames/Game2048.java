6dd768d56a0ef8a2ed96cbf3d2e5e40770546b5f
FranAM1 <franciscojosealmansa05@gmail.com>
2023-11-16 13:03:56 +0100

    Interfaz 2048

--- app/src/main/java/com/example/androidgames/Game2048.java
+++ app/src/main/java/com/example/androidgames/Game2048.java
@@ -2,7 +2,9 @@ package com.example.androidgames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Game2048 extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Game2048 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game2048);
+
+        findViewById(R.id.backButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backToTitle();
+            }
+        });
+    }
+
+    private void backToTitle() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
